595ea75149c1095e15ba41a1c53a572fb3674575
MrVester <makarspitsin@gmail.com>
2023-05-12 01:57:32 +0300

    Added level generator

---
+++ Assets/Scripts/LevelGenerator.cs
@@ -0,0 +1,58 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelGenerator : MonoBehaviour
+{
+    const float defaultRot = 11.25f;
+
+    public float offsetBetweenInstances = 0.1f;
+    public GameObject[] levelPrefabs;
+    public int startTonnelAmount = 6;
+    public List<GameObject> instances;
+    private int randomPrefab;
+    private int randomAngle;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        for (int i = -1; i < startTonnelAmount - 1; i++)
+        {
+            randomAngle = Random.Range(0, 7) * 45;
+            if (i == -1 || i == 0)
+            {
+                randomPrefab = 0;
+            }
+            else
+            {
+                randomPrefab = Random.Range(0, levelPrefabs.Length);
+            }
+
+            instances.Add(Instantiate(levelPrefabs[randomPrefab], new Vector3(0, 0, i * 20), Quaternion.Euler(new Vector3(0, 0, defaultRot + randomAngle))));
+
+            //Delete last added element
+            //instances.RemoveAt(instances.Count-1);
+        }
+        foreach (GameObject gameObject in instances)
+        {
+            gameObject.AddComponent<AddForce>();
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (instances.Count != 0 && instances[0].transform.position.z < -40)
+        {
+            randomAngle = Random.Range(0, 7) * 45;
+            randomPrefab = Random.Range(0, levelPrefabs.Length);
+            float posZofLastInstance = instances[instances.Count - 1].transform.position.z;
+            instances.Add(Instantiate(levelPrefabs[randomPrefab], new Vector3(0, 0, posZofLastInstance + 20 - instances[0].GetComponent<AddForce>().speed), Quaternion.Euler(new Vector3(0, 0, defaultRot + randomAngle))));
+            instances[instances.Count - 1].AddComponent<AddForce>();
+            Destroy(instances[0]);
+            instances.RemoveAt(0);
+        }
+    }
+}
